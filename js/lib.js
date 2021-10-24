f001df4a6731f7a78c9a7e88d790bb373045296a
ms0503 <ms0503@outlook.com>
2021-10-24 09:38:01 +0900

    Modify index.html:Prototype.jsを廃止。 Modify js/lib.js:クエリ文字列取得関数を実装。 Modify kodemari/index.html:Prototype.jsを廃止しjs/lib.jsのgetQueryParams関数を使用するよう変更。

--- js/lib.js
+++ js/lib.js
@@ -1,1 +1,12 @@
 'use strict';
+function getQueryParams(query) {
+    let result = {};
+    if(1 < query.length) {
+        let params = query.substring(1).split('&');
+        for(let i in params) {
+            let elem = params[i].split('=');
+            result[decodeURIComponent(elem[0])] = decodeURIComponent(elem[1]);
+        }
+    }
+    return result;
+}
